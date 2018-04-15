6e4cd66ca3629beb47f0a950a479aab4d7b6ab84
bglee <deptno@gmail.com>
2018-04-15 20:12:32 +0900

    update project

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,9 +1,19 @@
 import * as React from 'react'
-import {Container, Header} from 'semantic-ui-react'
+import {Container} from 'semantic-ui-react'
 import {Project} from './Project'
 
 export const Projects = props =>
   <Container>
+    <Project
+      header={{
+        title: 'Dynamon - 다이나모디비 GUI 클라이언트',
+        href: 'https://github.com/deptno/dynamon/release'
+      }}
+      description="다이나모디 디비의 데이터를 편하게 보자."
+      repos={[
+        {title:'Gihtub',href:'https://github.com/deptno/dynamon'},
+      ]}
+    />
     <Project
       header={{
         title: 'Clerk - 슬랙 저장소',
@@ -13,6 +23,16 @@ export const Projects = props =>
       repos={[
         {title:'프론트엔드',href:'https://github.com/deptno/slack-clerk-fe'},
         {title:'백엔드',href:'https://github.com/deptno/slack-clerk'}
+      ]}
+    />
+    <Project
+      header={{
+        title: 'Next.js typescript starter kit - Next.js + TypeScript',
+        href: '#'
+      }}
+      description="현재(https://bglee.me)의 시작 템플릿"
+      repos={[
+        {title:'Gihtub',href:'https://github.com/deptno/next.js-typescript-starter-kit'}
       ]}
     />
   </Container>
