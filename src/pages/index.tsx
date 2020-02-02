3f40f0610b66faae94f24da8ce6fcf0ce1b49f19
bglee <deptno@gmail.com>
2020-02-02 19:54:33 +0900

    feat: add pet/petitions

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,51 +4,56 @@ export const config = {amp: true}
 
 export default () => {
   return (
-    <div className="pa3">
-
-      <h4>contact</h4>
+    <div className="ph3 black-70">
+      <h2>contact</h2>
       <ul className="list pl0">
         <li>
-          <a href="http://twitter.com/@deptno" target="_blank">twitter</a>
+          <a className="link black-70" href="mailto:deptno@gmail.com" target="_blank">email</a>
         </li>
         <li>
-          <a href="mailto:deptno@gmail.com" target="_blank">email</a>
+          <a className="link black-70" href="http://twitter.com/@deptno" target="_blank">twitter</a>
         </li>
         <li>
-          <a href="http://deptno.slack.com" target="_blank">slack</a>
+          <a className="link black-70" href="http://deptno.slack.com" target="_blank">slack</a>
         </li>
       </ul>
-      <h4>as developer</h4>
+      <h2>writing</h2>
       <ul className="list pl0">
         <li>
-          <a href="https://github.com/deptno" target="_blank">github</a>
+          <a className="link black-70" href="https://deptno.github.io" target="_blank">blog</a>
         </li>
         <li>
-          <a href="https://www.npmjs.com/~deptno" target="_blank">npm</a>
-        </li>
-        <li>
-          <a href="https://deptno.github.io" target="_blank">blog</a>
-        </li>
-        <li>
-          <a href="http://medium.com/@deptno" target="_blank">medium</a>
+          <a className="link black-70" href="http://medium.com/@deptno" target="_blank">medium</a>
         </li>
       </ul>
-      <h4>project</h4>
+      <h2>code</h2>
       <ul className="list pl0">
         <li>
-          <a href="https://googit.io" target="_blank">googit.io</a>
+          <a className="link black-70" href="https://github.com/deptno" target="_blank">github</a>
         </li>
         <li>
-          <a href="https://tubemon.io" target="_blank">tubemon.io</a>
+          <a className="link black-70" href="https://www.npmjs.com/~deptno" target="_blank">npm</a>
         </li>
       </ul>
-      <h4>pet</h4>
+      <h2>pet</h2>
       <ul className="list pl0">
-        <li>
-          <a href="https://deptno.github.io/abandoned-animals" target="_blank">deptno.github.io/abandoned-animals</a>
+        <li id="petitions">
+          <a className="link black-70" href="https://deptno.github.io/petitions" target="_blank">deptno.github.io/petitions</a>
         </li>
         <li>
-          <a href="https://deptno.github.io/map-subway-storage" target="_blank">deptno.github.io/map-subway-storage</a>
+          <a className="link black-70" href="https://deptno.github.io/abandoned-animals" target="_blank">deptno.github.io/abandoned-animals</a>
+        </li>
+        <li>
+          <a className="link black-70" href="https://deptno.github.io/map-subway-storage" target="_blank">deptno.github.io/map-subway-storage</a>
+        </li>
+      </ul>
+      <h2>project</h2>
+      <ul className="list pl0">
+        <li>
+          <a className="link black-70" href="https://googit.io" target="_blank">googit.io</a>
+        </li>
+        <li>
+          <a className="link black-70" href="https://tubemon.io" target="_blank">tubemon.io</a>
         </li>
       </ul>
     </div>
